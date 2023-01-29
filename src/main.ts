151a736685c0bd7f730a221c7f3c32c3f8dc0422
Imran Nazir <imran.web.developer2021@gmail.com>
2023-01-29 12:17:26 +0600

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,31 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // const whitelist = ['http://localhost:3000', 'https://mobile-sell.vercel.app'];
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header(
+  //     'Access-Control-Allow-Methods',
+  //     'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  //   );
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+  //   next();
+  // });
+
+  // app.enableCors({
+  //   allowedHeaders: '*',
+  //   origin: '*',
+  // });
+  // app.enableCors();
+  // app.enableCors({
+  //   credentials: true,
+  //   origin: ['http://localhost:3000', 'https://mobile-sell.vercel.app'],
+  //   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+  // });
   app.enableCors();
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
