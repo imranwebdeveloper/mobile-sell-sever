e4688d45834eaba022e6099bd0142ecafa63dd8d
Imran Nazir <imran.web.developer2021@gmail.com>
2023-05-21 08:21:08 +0600

    test cors

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,13 @@ import * as admin from 'firebase-admin';
 import { config } from './config/configuration';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors({ origin: 'https://mobile-seller.vercel.app' });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+  app.enableCors({
+    origin: 'https://mobile-seller.vercel.app',
+    preflightContinue: true,
+  });
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   const { firebase, storageBucket } = config();
